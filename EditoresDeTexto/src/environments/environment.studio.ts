897bde21d72986f87de4124b1b2e055e084008e4
Patricio Arena <patricio.e.arena@gmail.com>
2020-03-27 10:38:18 -0300

    Ssl activado en apiFrontend

--- EditoresDeTexto/src/environments/environment.studio.ts
+++ EditoresDeTexto/src/environments/environment.studio.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   BASE_URL: 'https://localhost:4200',
-  API_URL: 'http://localhost:49266/api'
+  API_URL: 'https://localhost:44358/api'
 };
--- EditoresDeTexto/src/environments/environment.ts
+++ EditoresDeTexto/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   BASE_URL: 'https://localhost:4200',
-  API_URL: 'http://localhost:49266/api'
+  API_URL: 'https://localhost:44358/api'
 };
 
 /*
